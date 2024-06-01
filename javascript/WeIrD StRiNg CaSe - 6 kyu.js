8f374fcb67de1cd85190436953d095d75391e2e3
PythonCoder8 <72826534+PythonCoder8@users.noreply.github.com>
2024-06-01 10:21:41 -0700

    Create WeIrD StRiNg CaSe - 6 kyu.js

---
+++ javascript/WeIrD StRiNg CaSe - 6 kyu.js
@@ -0,0 +1,12 @@
+function toWeirdCase(string){
+    return string.split(' ')
+    .map(word => {
+      return word.split('')
+        .map((char, index) => {
+          if (index % 2 === 0) return char.toUpperCase()
+          return char
+        })
+        .join('')
+    })
+    .join(' ')
+}
