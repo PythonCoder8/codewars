ac07a1f14fd29fe48a92be9431aa19105bb0696f
PythonCoder8 <72826534+PythonCoder8@users.noreply.github.com>
2024-11-03 11:55:14 -0800

    Create Maximum Subarray Sum - 5 kyu.py

---
+++ python/Maximum Subarray Sum - 5 kyu.py
@@ -0,0 +1,9 @@
+def max_sequence(arr):
+    max_sum = 0
+    current_sum = 0
+    for num in arr:
+        current_sum += num
+        if current_sum < 0:
+            current_sum = 0
+        max_sum = max(max_sum, current_sum)
+    return max_sum
